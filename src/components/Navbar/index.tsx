f6eedf7e2a31450c6d596879e5c09ace5fcfab5b
Belguith Omar <omar.belguith@medtech.tn>
2023-02-03 10:56:48 +0000

    removing navabr

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -63,28 +63,8 @@ export default function Nav() {
                   variant={'link'}
                   cursor={'pointer'}
                   minW={0}>
-                  <Avatar
-                    size={'sm'}
-                    src={'https://avatars.dicebear.com/api/male/username.svg'}
-                  />
                 </MenuButton>
                 <MenuList alignItems={'center'}>
-                  <br />
-                  <Center>
-                    <Avatar
-                      size={'2xl'}
-                      src={'https://avatars.dicebear.com/api/male/username.svg'}
-                    />
-                  </Center>
-                  <br />
-                  <Center>
-                    <p>Username</p>
-                  </Center>
-                  <br />
-                  <MenuDivider />
-                  <MenuItem>Your Servers</MenuItem>
-                  <MenuItem>Account Settings</MenuItem>
-                  <MenuItem>Logout</MenuItem>
                 </MenuList>
               </Menu>
             </Stack>
